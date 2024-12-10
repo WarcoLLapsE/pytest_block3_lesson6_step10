647233fb7b33ade1d1e2bd904f625ccd478e374f
WarcoLLapsE <warcollapse@gmail.com>
2024-12-10 21:15:03 +0300

    Added test_items.py

--- test_items.py
+++ test_items.py
@@ -1,8 +1,11 @@
+import time
+
 from selenium.webdriver.common.by import By
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_guest_should_see_login_link(browser):
+def test_check_button_add_to_basket(browser):
     browser.get(link)
-    browser.find_element(By.CSS_SELECTOR, "#login_link")
+    time.sleep(30)
+    assert browser.find_element(By.CSS_SELECTOR, ".btn.btn-lg.btn-primary.btn-add-to-basket")
